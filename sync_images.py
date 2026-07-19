e85fb35c658dd84e4eeb30a1330fb20ae4b36403
TEMU Auto <admin@temu-auto.com>
2026-07-19 22:26:42 +0800

    fix: sync_images移除未使用的BASE_URL导入

--- sync_images.py
+++ sync_images.py
@@ -13,7 +13,7 @@ import tempfile
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from data_source.feishu import FeishuDataSource, BASE_URL
+from data_source.feishu import FeishuDataSource
 from utils.image_uploader import create_uploader
 from config.settings import (
     FEISHU_BASE_TOKEN, IMAGE_BED_CONFIG,
